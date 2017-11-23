1fec9469919e1a7eb367783bfe222a3d91c0d771
walter <1204487745@qq.com>
2017-11-23 22:27:55 +0800

    成员变量和局部变量

---
+++ day09/src/cn/walter/demo02/Car.java
@@ -0,0 +1,15 @@
+package cn.walter.demo02;
+/*
+ */
+public class Car {
+	String color;
+	int count;
+	public void run(){
+		int x = 1;
+		for (int i = 0; i < 4; i++) {
+			
+		}
+		
+		System.out.println("小汽车在跑！"+"颜色是"+color+",有"+count+"个轮子");
+	}
+}
