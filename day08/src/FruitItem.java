964cf808ab78c4cd0ffda6b4ea758c15b59f9cfc
walter <1204487745@qq.com>
2017-11-23 20:14:02 +0800

    -m

---
+++ day08/src/FruitItem.java
@@ -0,0 +1,23 @@
+/*
+ * 自定义类，描述商品信息
+ * 商品的属性
+ * 	编号，int
+ * 	品名，String
+ *	价格，Double
+ *	数量，int
+ *	总价，Double
+ *
+ *	商品类型，存储到ArrayList中
+ * */
+public class FruitItem {
+	//商品编号
+	int ID;
+	//商品名称
+	String name;
+	//商品价格
+	Double price;
+	//商品数量
+	int number;
+	//商品总价
+	double money;
+}
