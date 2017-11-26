33561d88fa2b1a9e3e7927d492ff8940eb51db22
walter <1204487745@qq.com>
2017-11-26 22:14:24 +0800

    方法的重写在派生类中对基类方法的重写

---
+++ day10/src/cn/walter/demo04/Fu.java
@@ -0,0 +1,7 @@
+package cn.walter.demo04;
+
+public class Fu {
+	public void show(){
+		System.out.println("父类的show1方法");
+	}
+}
---
+++ day10/src/cn/walter/demo04/Zi.java
@@ -0,0 +1,8 @@
+package cn.walter.demo04;
+
+public class Zi extends Fu {
+	
+	public void show(){
+		System.out.println("子类的show2方法");
+	}
+}
