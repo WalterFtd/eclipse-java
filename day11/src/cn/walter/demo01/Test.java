a6dc7fe763ea1b54d063a0e4187895702d2c166e
walter <1204487745@qq.com>
2017-11-27 19:06:45 +0800

    接口基础，更抽象的抽象类

---
+++ day11/src/cn/walter/demo01/Test.java
@@ -0,0 +1,11 @@
+package cn.walter.demo01;
+/*
+ * 接口的测试类
+ */
+public class Test {
+	public static void main(String[] args) {
+		//创建接口的实现类的对象
+		MyInterfaceImpl my = new MyInterfaceImpl();
+		my.function();
+	}
+}
