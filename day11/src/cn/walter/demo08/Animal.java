c6e985c1f7ac0c93a67aff988fb68423e2c91be7
walter <1204487745@qq.com>
2017-12-01 21:41:39 +0800

    多态转型的案例

---
+++ day11/src/cn/walter/demo08/Animal.java
@@ -0,0 +1,5 @@
+package cn.walter.demo08;
+
+public abstract class Animal {
+	public abstract void eat();
+}
---
+++ day11/src/cn/walter/demo08/Cat.java
@@ -0,0 +1,11 @@
+package cn.walter.demo08;
+
+public class Cat extends Animal {
+	public void eat() {
+		System.out.println("Ã¨³ÔÃ¨Á¸");
+	}
+	public void catchMouse(){
+		System.out.println("Ã¨×¥ÀÏÊó");
+	}
+
+}
